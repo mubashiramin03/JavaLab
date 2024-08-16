77d2ae99e21d5f8c8d8dd5538cd140fc7624dba2
mubashiramin03 <119844755+mubashiramin03@users.noreply.github.com>
2024-08-16 20:53:02 +0530

    Create bank.java

---
+++ bank.java
@@ -0,0 +1,11 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited $" + amount);
+        System.out.println("New balance:
